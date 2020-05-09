e16c23145c6a3ac0d2179ea0258d51c025e53cc1
Zac White <zacpwhite@gmail.com>
2020-05-09 15:50:42 -0400

    Added migrations

---
+++ Data/Migrations/20200509191248_InitalMigration.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Jeopardy.Data.Migrations
+{
+    public partial class InitalMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                schema: "dbo",
+                columns: table => new
+                {
+                    GameId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GameTitle = table.Column<string>(nullable: true),
+                    GameDescription = table.Column<string>(nullable: true),
+                    CurrentRound = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.GameId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                schema: "dbo",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryTitle = table.Column<string>(nullable: true),
+                    CategoryDescription = table.Column<string>(nullable: true),
+                    CategorySortOrder = table.Column<int>(nullable: false),
+                    Round = table.Column<int>(nullable: false),
+                    GameId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                    table.ForeignKey(
+                        name: "FK_Categories_Games_GameId",
+                        column: x => x.GameId,
+                        principalSchema: "dbo",
+                        principalTable: "Games",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                schema: "dbo",
+                columns: table => new
+                {
+                    AnswerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AnswerValue = table.Column<int>(nullable: false),
+                    AnswerText = table.Column<string>(nullable: true),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.AnswerId);
+                    table.ForeignKey(
+                        name: "FK_Answers_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalSchema: "dbo",
+                        principalTable: "Categories",
+                        principalColumn: "CategoryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                schema: "dbo",
+                columns: table => new
+                {
+                    QuestionId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestionText = table.Column<string>(nullable: true),
+                    IsCorrect = table.Column<bool>(nullable: false),
+                    AnswerId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.QuestionId);
+                    table.ForeignKey(
+                        name: "FK_Questions_Answers_AnswerId",
+                        column: x => x.AnswerId,
+                        principalSchema: "dbo",
+                        principalTable: "Answers",
+                        principalColumn: "AnswerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_CategoryId",
+                schema: "dbo",
+                table: "Answers",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_GameId",
+                schema: "dbo",
+                table: "Categories",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_AnswerId",
+                schema: "dbo",
+                table: "Questions",
+                column: "AnswerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Questions",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Answers",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Categories",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Games",
+                schema: "dbo");
+        }
+    }
+}
